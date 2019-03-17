856c42d1927a264bd1839121e6f02575d47e4529
gregouz3 <gregoire.defleurac34@gmail.com>
2019-03-17 14:59:53 +0100

    ajout table substitute, clées étrangeres

--- database_food.sql
+++ database_food.sql
@@ -1,27 +1,33 @@
+DROP DATABASE IF EXISTS database_food;
 CREATE DATABASE database_food CHARACTER SET 'utf8';
-USE database_food
+USE database_food;
 
-CREATE TABLE Food_category (
-  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  product_name VARCHAR(255) UNIQUE,
+  CREATE TABLE Food_category (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    category_name VARCHAR(255) NOT NULL UNIQUE
 
-)ENGINE=INNODB;
+  )ENGINE=INNODB;
 
-CREATE TABLE Product_data (
-  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  product_name VARCHAR(255) UNIQUE,
-  url VARCHAR(2000),
-  code BIGINT UNSIGNED NOT NULL UNIQUE,
-  ingredients TEXT,
-  stores TEXT,
-  nutrition_grades VARCHAR(1),
-  food_category_id INT UNSIGNED,
-  food_substitute_id INT UNSIGNED,
+  CREATE TABLE Food_product (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    category_id INT UNSIGNED NOT NULL,
+    product_name VARCHAR(255) NOT NULL,
+    url VARCHAR(255) NOT NULL,
+    code BIGINT UNSIGNED NOT NULL,
+    ingredients TEXT,
+    stores TEXT,
+    nutrition_grade VARCHAR(1) NOT NULL
 
-  ADD CONSTRAINT 'fk_food_category'
-    FOREIGN KEY ('food_category_id') REFERENCES Food_category(id) ON DELETE CASCADE,
-  ADD CONSTRAINT 'fk_food_substitute_id'
-    FOREIGN KEY ('food_substitute_id') REFERENCES Product_data(id) ON DELETE SET NULL
+  )ENGINE=INNODB;
 
-)ENGINE=INNODB;
+  CREATE TABLE Food_substitute (
+    product_source_id INT UNSIGNED NOT NULL,
+    substitute_id INT UNSIGNED NOT NULL
 
+  )ENGINE=INNODB;
+
+  ALTER TABLE Food_product
+  ADD CONSTRAINT fk_category_id FOREIGN KEY (category_id) REFERENCES Food_category(id);
+  ALTER TABLE Food_substitute
+  ADD CONSTRAINT fk_product_source_id FOREIGN KEY (product_source_id) REFERENCES Food_product(id),
+  ADD CONSTRAINT fk_substitute_id FOREIGN KEY (substitute_id) REFERENCES Food_product(id);
