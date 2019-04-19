2871ecebc2bc95827043d653754f22504aed646b
gregouz3 <gregoire.defleurac34@gmail.com>
2019-04-19 15:20:47 +0200

    insertion des categories dans la db

--- classes.py
+++ classes.py
@@ -1,6 +1,57 @@
+#!/usr/bin/python3
+
+# -*- coding: Utf-8 -*
+
 import json
 import requests
 import mysql.connector
-from config import *
+from constantes import *
 
 
+
+class Category:
+
+  def __init__(self):
+
+    self.request_category = ""
+    self.category = list()
+    self.read_config_cat()
+    self.get_category_data()
+    self.insert_category_data()
+
+  def read_config_cat(self):
+
+    connector = mysql.connector.connect(**config)
+    cur = connector.cursor()
+    json_file = open("config_datafood.json")
+    data_category = json.load(json_file)
+    self.request_category = data_category['list_category']
+
+  def get_category_data(self):
+
+    get_category = requests.get(self.request_category)
+    data = get_category.json()
+    for dico_cat in data['tags']:
+      if len(dico_cat['name']) < 100:
+        categories = {"name": dico_cat['name'], "id": dico_cat['id'] }
+        self.category.append(categories)
+
+  def insert_category_data(self):
+
+    cnx = mysql.connector.connect(**config)
+    cursor = cnx.cursor()
+    i = 0
+    for cat in self.category:
+      cat_replace = cat['name'].replace("'","''")
+      add_category = (f"INSERT INTO Food_category (category_name) VALUES ('{cat_replace}')")
+      print(add_category)
+      cursor.execute(add_category)
+      i += 1
+      if i > 4:
+        break
+
+    cnx.commit()
+    cursor.close()
+
+if __name__ == '__main__':
+  Category()
--- constantes.py
+++ constantes.py
@@ -1,4 +1,6 @@
+#!/usr/bin/python3
 
+# -*- coding: Utf-8 -*
 import mysql.connector
 
 config = {
