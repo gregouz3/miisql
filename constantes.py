ae67f7b0b063a41a1f5730035a61aeae09f74548
gregouz3 <gregoire.defleurac34@gmail.com>
2019-04-25 15:54:14 +0200

    ajout constantes insertion cat et prod

--- constantes.py
+++ constantes.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 # -*- coding: Utf-8 -*
+
 import mysql.connector
 
 config = {
@@ -11,5 +12,7 @@ config = {
   'raise_on_warnings': True
 }
 
+add_category = (f"INSERT INTO Food_category (category_name) VALUES ('{cat_replace}')")
 
+add_product = ("""INSERT INTO Food_product(product_name, nutriscore, category, url) VALUES (%s, %s, %s, %s)""")
 
