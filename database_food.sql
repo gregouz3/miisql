7c63cb184d96f9f67cec780d48bc019419088f80
gregouz3 <gregoire.defleurac34@gmail.com>
2019-05-06 19:15:49 +0200

    ajout colonne store

--- database_food.sql
+++ database_food.sql
@@ -1,33 +1,33 @@
 DROP DATABASE IF EXISTS database_food;
-DROP TABLE Food_category;
 CREATE DATABASE database_food CHARACTER SET 'utf8';
 USE database_food;
 GRANT ALL PRIVILEGES ON database_food.* TO 'P5'@'localhost';
 
   CREATE TABLE Food_category (
-    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    category_name VARCHAR(255) NOT NULL UNIQUE
-
+    id INT UNSIGNED AUTO_INCREMENT,
+    category_name VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
   )ENGINE=INNODB;
 
   CREATE TABLE Food_product (
-    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    category_id INT DEFAULT NULL,
+    id INT UNSIGNED AUTO_INCREMENT,
     category VARCHAR(255) DEFAULT NULL,
     product_name VARCHAR(255) NOT NULL,
     url VARCHAR(255) NOT NULL,
-    nutriscore VARCHAR(1)  DEFAULT NULL
-
+    nutriscore VARCHAR(1)  DEFAULT NULL,
+    store VARCHAR(255) DEFAULT NULL,
+    PRIMARY KEY (id)
   )ENGINE=INNODB;
+
 
   CREATE TABLE Food_substitute (
-    product_source_id INT UNSIGNED NOT NULL,
-    substitute_id INT UNSIGNED NOT NULL
-
+    id INT UNSIGNED AUTO_INCREMENT,
+    substitute_name VARCHAR(255) DEFAULT NULL,
+    substitute_id_cat INT UNSIGNED NULL,
+    substitute_id_prod INT UNSIGNED NOT NULL,
+    PRIMARY KEY (id)
   )ENGINE=INNODB;
 
-  ALTER TABLE Food_product
-  ADD CONSTRAINT fk_category_id FOREIGN KEY (category_id) REFERENCES Food_category(id);
   ALTER TABLE Food_substitute
-  ADD CONSTRAINT fk_product_source_id FOREIGN KEY (product_source_id) REFERENCES Food_category(id),
-  ADD CONSTRAINT fk_substitute_id FOREIGN KEY (substitute_id) REFERENCES Food_product(id);
+  ADD CONSTRAINT fk_category_source_id FOREIGN KEY (substitute_id_cat) REFERENCES Food_category(id),
+  ADD CONSTRAINT fk_product_source_id FOREIGN KEY (substitute_id_prod) REFERENCES Food_product(id);
