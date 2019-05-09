e5108460a727667ef7ae7f1253a8e8552b480baa
gregouz3 <gregoire.defleurac34@gmail.com>
2019-05-09 15:48:50 +0200

    ajout f fill_cat

--- fill_database.py
+++ fill_database.py
@@ -2,9 +2,10 @@ import json
 import requests
 import mysql.connector
 from classes import *
+from constantes import *
 
 
-def read_config_cat_prod(category):
+def read_config_prod(category):
 
     url_cat = 'https://fr.openfoodfacts.org/categorie'
     nb_page = 1
@@ -34,17 +35,28 @@ def read_config_cat_prod(category):
         url = prods["url"]
         store = prods.get("stores", None)
         product.add_database(products_name, nutriscore_level, categories_names, url, store)
+
       nb_page += 1
       nb_pages -= 1
+
+def read_config_cat_prod():
+
+  cnx = mysql.connector.connect(**config)
+  cursor = cnx.cursor()
+  cursor.execute(fill_cat)
+  print(fill_cat)
+  cnx.commit()
+  cursor.close()
+
 
 
 if __name__ == '__main__':
 
+
   cnx = mysql.connector.connect(**config)
   cursor = cnx.cursor()
-
-  read_config_cat_prod("Boissons")
-  read_config_cat_prod("Viandes")
+  read_config_prod("Viandes")
+  read_config_cat_prod()
 
   cnx.commit()
   cursor.close()
