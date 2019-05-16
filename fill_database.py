f94ab3c0e9edad2076f975ec94009d587d2bc89f
gregouz3 <gregoire.defleurac34@gmail.com>
2019-05-16 15:26:55 +0200

    ajout para cat_id

--- fill_database.py
+++ fill_database.py
@@ -5,38 +5,36 @@ from classes import *
 from constantes import *
 
 
-def read_config_prod(category):
+def read_config_prod(category, category_id):
 
     url_cat = 'https://fr.openfoodfacts.org/categorie'
-    nb_page = 1
+    nb_pages_url = 1
     url_json = 'json'
-    url = "{}/{}/{}.{}".format(url_cat, category, nb_page, url_json)
+    url = "{}/{}/{}.{}".format(url_cat, category, nb_pages_url, url_json)
     request_url = requests.get(url)
     products = request_url.json()
 
-    nb_pages = 3
+    nb_pages = 5
     while nb_pages > 0:
 
-      url = "{}/{}/{}.{}".format(url_cat, category, nb_page, url_json)
+      url = "{}/{}/{}.{}".format(url_cat, category, nb_pages_url, url_json)
       request_url = requests.get(url)
       products = request_url.json()
       print(url)
       i = 0
       products_name = ""
       nutriscore_level = ""
-      categories_names = ""
       store = ""
       product = Product()
       for prods in products["products"]:
 
         products_name = prods.get("product_name_fr", prods.get("product_name"))
         nutriscore_level = prods.get("nutrition_grades", None)
-        categories_names = prods["pnns_groups_2"]
         url = prods["url"]
         store = prods.get("stores", None)
-        product.add_database(products_name, nutriscore_level, categories_names, url, store)
+        product.add_database(products_name, nutriscore_level, url, store, category_id)
 
-      nb_page += 1
+      nb_pages_url += 1
       nb_pages -= 1
 
 def read_config_cat_prod():
@@ -55,8 +53,9 @@ if __name__ == '__main__':
 
   cnx = mysql.connector.connect(**config)
   cursor = cnx.cursor()
-  read_config_prod("Viandes")
   read_config_cat_prod()
+  read_config_prod("Viandes", 1)
+  read_config_prod("Boissons", 2)
 
   cnx.commit()
   cursor.close()
