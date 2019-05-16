74ab0402d8e05812d16bba4628f5daeacde9d9a5
gregouz3 <gregoire.defleurac34@gmail.com>
2019-05-16 15:23:38 +0200

    ajout selection cat_prod

--- constantes.py
+++ constantes.py
@@ -12,8 +12,10 @@ config = {
   'raise_on_warnings': True
 }
 
-add_product = ("""INSERT INTO Food_product(product_name, nutriscore, category, url, store) VALUES (%s, %s, %s, %s, %s)""")
+add_product = ("""INSERT INTO Food_product(product_name, nutriscore, url, store, category_id) VALUES (%s, %s, %s, %s, %s)""")
 
-fill_cat = ("""INSERT INTO Food_category (category_name) SELECT DISTINCT category FROM Food_product""")
+fill_cat = ("""INSERT INTO Food_category (category_name) VALUES ("Viandes"), ("Boissons")""")
 
-
+select_cat = ("""SELECT * FROM Food_category""")
+select_cat_prod_1 = ("""SELECT Food_product.id_prod, Food_product.product_name, Food_product.nutriscore FROM Food_product WHERE id_prod < 101""")
+select_cat_prod_2 = ("""SELECT Food_product.id_prod, Food_product.product_name, Food_product.nutriscore FROM Food_product WHERE id_prod > 100""")
