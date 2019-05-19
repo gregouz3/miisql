66817f4986526f808d0b13f2bb21fd9dfe9dc41d
gregouz3 <gregoire.defleurac34@gmail.com>
2019-05-19 21:45:57 +0200

    ajout selection cat, prod

--- constantes.py
+++ constantes.py
@@ -14,8 +14,13 @@ config = {
 
 add_product = ("""INSERT INTO Food_product(product_name, nutriscore, url, store, category_id) VALUES (%s, %s, %s, %s, %s)""")
 
-fill_cat = ("""INSERT INTO Food_category (category_name) VALUES ("Viandes"), ("Boissons")""")
+fill_cat = ("""INSERT INTO Food_category (category_name) VALUES ("Sandwichs"), ("Boissons")""")
 
 select_cat = ("""SELECT * FROM Food_category""")
-select_cat_prod_1 = ("""SELECT Food_product.id_prod, Food_product.product_name, Food_product.nutriscore FROM Food_product WHERE id_prod < 101""")
-select_cat_prod_2 = ("""SELECT Food_product.id_prod, Food_product.product_name, Food_product.nutriscore FROM Food_product WHERE id_prod > 100""")
+
+select_cat_prod = ("SELECT id_prod, product_name, nutriscore FROM Food_product INNER JOIN Food_category ON Food_product.category_id = Food_category.id_cat WHERE Food_category.id_cat = %s")
+
+select_prod = ("""SELECT id_prod, product_name, nutriscore, store, url FROM Food_product WHERE Food_product.id_prod = %s""")
+
+
+
