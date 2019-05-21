43696c52797e00a5caccb96dc38eff786a63e0ff
gregouz3 <gregoire.defleurac34@gmail.com>
2019-05-21 18:18:50 +0200

    correction texte

--- OpenFoodSubstitute.py
+++ OpenFoodSubstitute.py
@@ -31,7 +31,16 @@ def choose_product(id_prod):
   cursor = cnx.cursor()
   cursor.execute(select_prod, id_prod)
   for id_prod, product_name, nutriscore, store, url in cursor:
-    print('\n Vous avez choisi : ' ,product_name, ', son nutriscore est de ', nutriscore, ', vous pouvez trouver ce produit ici --> ', store, ',\n et voici son url pour plus dinfo :', url)
+    print('\n substitutuion en cours lol : ')
+
+
+def substitute_product(id_prod):
+
+  cnx = mysql.connector.connect(**config)
+  cursor = cnx.cursor()
+  cursor.execute(select_prod_subs, fill_substitute, id_prod)
+  for substitute_name, nutriscore, store, url in cursor:
+    print('\n Voici un substitut, un aliment plus sein pour votre santé ! : ', substitute_name, ',son nutriscore est de : ', nutriscore, ', vous pouvez acheter ce produit à ', store, ',\n Lien vers OpenFoodFact :', url)
 
 
 
@@ -41,18 +50,15 @@ if __name__ == '__main__':
   while loop:
       cnx = mysql.connector.connect(**config)
       cursor = cnx.cursor()
-      proposals = int(input("1- Looking for a product to substitute? \n"
-                            "2-  \n"
-                            "3- Exit -OpenFoodSubstitute-\n"
-                            "Select a proposal :  "))
+      proposals = int(input("\n1- Quel aliment souhaitez-vous remplacer ? \n2- Retrouver mes aliments substitués.\n3- Exit \n -OpenFoodSubstitute-\n"))
       if proposals == 3:
         loop = False
 
       if proposals == 1:
         display_category()
-        category_choose = (int(input("Select a category: ")),)
+        category_choose = (int(input("Selectionnez la catégorie : ")),)
         choose_category(category_choose)
-        id_prod = (int(input("Select a product: ")),)
+        id_prod = (int(input("Selectionnez un aliment: ")),)
         choose_product(id_prod)
         loop = False
       cnx.commit()
