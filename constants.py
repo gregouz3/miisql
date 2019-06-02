c3f3c4d6950d3c98b600f12f8cba0394954bf54a
gregouz3 <gregoire.defleurac34@gmail.com>
2019-06-02 16:10:17 +0200

    commentaires et pep8

--- constants.py
+++ constants.py
@@ -1,25 +1,42 @@
-import mysql.connector
+#!/usr/bin/python3
 
-config = {
-  'user' : 'P5',
-  'password':'password',
-  'host': 'localhost',
-  'database': 'database_food'
+# -*- coding: Utf-8 -*
+
+"""
+File containing constants and queries
+"""
+
+# Connection configuration to the database
+CONFIG = {
+    'user' : 'P5', 'password' : 'password', 'host' : 'localhost', 'database' : 'database_food'
 }
 
-add_product = ("""INSERT INTO Food_product(product_name, nutriscore, url, store, category_id) VALUES (%s, %s, %s, %s, %s)""")
+ADD_PRODUCT = ("""INSERT INTO Food_product
+               (product_name, nutriscore, url, store, category_id)
+                VALUES (%s, %s, %s, %s, %s)""")
 
-fill_cat = ("""INSERT INTO Food_category (category_name) VALUES ("Volailles"), ("Sandwichs"), ("Compotes") """)
+FILL_CAT = ("""INSERT INTO Food_category
+            (category_name) VALUES ("Volailles"), ("Sandwichs"), ("Compotes")""")
 
-select_cat = ("""SELECT * FROM Food_category""")
+SELECT_CAT = ("""SELECT * FROM Food_category""")
 
-select_cat_prod = ("""SELECT id_prod, product_name, nutriscore FROM Food_product INNER JOIN Food_category ON Food_product.category_id = Food_category.id_cat WHERE Food_category.id_cat = %s""")
+# Query which return a list of the product according to the category chosen
+SELECT_CAT_PROD = ("""SELECT id_prod, product_name, nutriscore FROM Food_product
+                   INNER JOIN Food_category
+                   ON Food_product.category_id = Food_category.id_cat
+                   WHERE Food_category.id_cat = %s""")
 
-select_prod = ("""SELECT Food_product.id_prod, Food_product.category_id, Food_product.product_name, Food_product.nutriscore, Food_product.store, Food_product.url FROM Food_product WHERE  Food_product.id_prod = %s""")
+SELECT_PROD = ("""SELECT Food_product.id_prod, Food_product.category_id, Food_product.product_name,
+               Food_product.nutriscore, Food_product.store, Food_product.url FROM Food_product
+               WHERE  Food_product.id_prod = %s""")
 
-fill_substitute = ("""INSERT INTO Food_substitute(category_id, product_id, substitute_name, nutriscore, store, url) VALUES (%s, %s, %s, %s, %s, %s)""")
+FILL_SUBSTITUTE = ("""INSERT INTO Food_substitute
+                   (category_id, product_id, substitute_name, nutriscore, store, url)
+                   VALUES (%s, %s, %s, %s, %s, %s)""")
 
-select_sub_store = ("""SELECT category_id, id_prod, product_name, nutriscore, store, url FROM Food_product WHERE category_id = %s AND nutriscore = 'a' AND store <> '' """)
+# Query which return a substitute with a store
+SELECT_SUB_STORE = ("""SELECT category_id, id_prod, product_name, nutriscore, store, url
+                    FROM Food_product WHERE category_id = %s
+                    AND nutriscore = 'a' AND store <> '' LIMIT 1""")
 
-select_subs = ("""SELECT id_subs, category_id, product_id, substitute_name, nutriscore, store, url FROM Food_substitute """)
-
+SELECT_SUBS = ("""SELECT * FROM Food_substitute """)
