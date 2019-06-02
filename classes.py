52b2ff131b65638ec0c391057d9709164401c552
gregouz3 <gregoire.defleurac34@gmail.com>
2019-06-02 16:13:40 +0200

    commentaires et pep 8

--- classes.py
+++ classes.py
@@ -2,101 +2,107 @@
 
 # -*- coding: Utf-8 -*
 
+"""
+File containing the classes Category and Product
+"""
+
 import json
 import requests
 import mysql.connector
-from constants import *
-
-class Category:
-
-  def __init__(self):
-
-    self.request_category = ""
-    self.category = list()
-
-    self.read_config_cat()
-    self.get_category_data()
-    self.insert_category_data()
-
-  def read_config_cat(self):
-
-    connector = mysql.connector.connect(**config)
-    cur = connector.cursor()
-    json_file = open("config_datafood.json")
-    data_category = json.load(json_file)
-    self.request_category = data_category['list_category']
-
-  def get_category_data(self):
-
-    get_category = requests.get(self.request_category)
-    data = get_category.json()
-    for dico_cat in data['tags']:
-      if len(dico_cat['name']) < 100:
-        categories = {"name": dico_cat['name'], "id": dico_cat['id'] }
-        self.category.append(categories)
-
-  def insert_category_data(self):
-
-    cnx = mysql.connector.connect(**config)
-    cursor = cnx.cursor()
-    i = 0
-    for cat in self.category:
-      cat_replace = cat['name'].replace("'","''")
-      add_category = (f"INSERT INTO Food_category (category_name) VALUES ('{cat_replace}')")
-      cursor.execute(add_category)
-      i += 1
-      if i > 9:
-        break
-    cnx.commit()
-    cursor.close()
-
-class Product:
-
-  def __init__(self):
-
-    self.products_name = ""
-    self.nutriscore_level = ""
-    self.url = ""
-    self.store = ""
-    self.id_prod = 0
-    self.category_id = 1
+from constants import CONFIG, ADD_PRODUCT, SELECT_PROD
 
 
+class Category():
+    '''The class category. Recover Openfoodfact Categories'''
 
-  def add_database(self, products_name, nutriscore_level, url, store, category_id):
+    def __init__(self):
 
-      self.products_name = products_name
-      self.nutriscore_level = nutriscore_level
-      self.url = url
-      self.store = store
-      self.category_id = category_id
+        self.request_category = ""
+        self.category = list()
 
-      cnx = mysql.connector.connect(**config)
-      cursor = cnx.cursor()
-      data_products = (self.products_name, self.nutriscore_level,self.url, self.store, self.category_id)
+    def read_config_cat(self):
+        '''Read config json'''
+        json_file = open("config_datafood.json")
+        data_category = json.load(json_file)
+        self.request_category = data_category['list_category']
 
-      cursor.execute(add_product, data_products)
+    def get_category_data(self):
+        '''get categories json and fill the list category'''
+        get_category = requests.get(self.request_category)
+        data = get_category.json()
+        for dico_cat in data['tags']:
+            if len(dico_cat['name']) < 100:
+                categories = {"name": dico_cat['name'], "id": dico_cat['id']}
+                self.category.append(categories)
 
-      cnx.commit()
-      cursor.close()
+    def insert_category_data(self):
+        '''Insert category in the table Food_category'''
+        cnx = mysql.connector.connect(**CONFIG)
+        cursor = cnx.cursor()
+        nb_cat = 0
+        for cat in self.category:
+            cat_replace = cat['name'].replace("'", "''")
+            add_category = ("""INSERT INTO Food_category (category_name)"
+                            " VALUES ('{cat_replace}')""")
+            cursor.execute(add_category)
+            nb_cat += 1
+            # For 10 categories
+            if nb_cat > 9:
+                break
+        cnx.commit()
+        cursor.close()
 
-  def product_selected(self, p_id):
 
-      cnx = mysql.connector.connect(**config)
-      cursor = cnx.cursor()
-      cursor.execute(select_prod, p_id)
+class Product():
+    '''The class Product . For the filling a table Food_product
+     and create the product object chosen by the user'''
 
-      for id_prod, category_id, product_name, nutriscore, store, url in cursor:
-          self.id_prod = id_prod
-          self.category_id = category_id
-          self.products_name = product_name
-          self.nutriscore_level = nutriscore
-          self.url = url
-          self.store = store
+    def __init__(self):
+        '''The constructor is empty'''
+        self.products_name = ""
+        self.nutriscore_level = ""
+        self.url = ""
+        self.store = ""
+        self.id_prod = 0
+        self.category_id = 1
 
-  def display_product(self):
+    def add_database(self, products_name, nutriscore_level, url, store, category_id):
+        '''Function used to fill the table Food_product, used in  fill_database.py program'''
+        self.products_name = products_name
+        self.nutriscore_level = nutriscore_level
+        self.url = url
+        self.store = store
+        self.category_id = category_id
 
-      print("\n Vous avez sélectionné : {} de la categorie n° {}, son nutriscore est de : {}.\n Ce produit est en vente à {} .\n Le lien OpenFoodFact : {} \n".format(self.products_name, self.category_id, self.nutriscore_level, self.store, self.url))
-if __name__ == '__main__':
-  Category()
-  Product()
+        cnx = mysql.connector.connect(**CONFIG)
+        cursor = cnx.cursor()
+        data_products = (self.products_name, self.nutriscore_level, self.url, self.store,
+                         self.category_id)
+
+        cursor.execute(ADD_PRODUCT, data_products)
+
+        cnx.commit()
+        cursor.close()
+
+    def product_selected(self, id_product):
+        '''Create a object with data to the id_product selected by the user'''
+        cnx = mysql.connector.connect(**CONFIG)
+        cursor = cnx.cursor()
+        cursor.execute(SELECT_PROD, id_product)
+
+        for id_prod, category_id, product_name, nutriscore, store, url in cursor:
+            self.id_prod = id_prod
+            self.category_id = category_id
+            self.products_name = product_name
+            self.nutriscore_level = nutriscore
+            self.url = url
+            self.store = store
+
+    def display_product(self):
+        '''Display data belong to product'''
+        print("\n Vous avez sélectionné : {} de la categorie n° {}"
+              ", son nutriscore est de : {}.\n"
+              " Ce produit est en vente à {} .\n"
+              "Le lien OpenFoodFact : {}\n"
+              .format(self.products_name, self.category_id, self.nutriscore_level, self.store,
+                      self.url))
